27d415909bce1058b833fc4d563258ac17859685
Zinamation <starlit.zhao@digipen.edu>
2025-09-25 12:48:46 -0700

    Added an audio manager
    Added comments to collectible script, added an audio manager for playing sounds

--- Project0918/Assets/Scripts/CollectibleLogic.cs
+++ Project0918/Assets/Scripts/CollectibleLogic.cs
@@ -5,20 +5,26 @@ public class CollectibleLogic : MonoBehaviour
 {
     public int scoreValue;
     PlayerController playerScript;
+    AudioSource audioSource;
 
     void Start()
     { 
         playerScript = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
-        if (!playerScript)
-        {
-            Debug.Log("Script null!");
-        }
+        audioSource = GameObject.Find("Audio Manager").GetComponent<AudioSource>();
     }
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player") == true)
         {
+
+            // Currently this adds points to the Player object by finding the tag "Player"
+            // then adds the value of this collectible to the player's score
             playerScript.pointValue += scoreValue;
+
+            // Currently calls a game object called "Audio Manager" and sends a play signal
+            audioSource.Play();
+
+            // Destroys this object
             Destroy(this.gameObject);
         }
     }
