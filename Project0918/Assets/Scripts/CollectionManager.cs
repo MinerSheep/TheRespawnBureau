1136293d600fa8ce43928ecbaddfdba32565b834
donghyunkang-digipen <donghyunkang-digipen@users.noreply.github.com>
2025-10-09 14:10:25 -0700

    Fixed bug in the High Score
    1) Fixed bug which doesn't update the high score.
    2) 'B' for reset the High score.

--- Project0918/Assets/Scripts/CollectionManager.cs
+++ Project0918/Assets/Scripts/CollectionManager.cs
@@ -23,17 +23,40 @@ public class CollectionManager : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log("Current Score: " + score);
+
         // HUD updates if the player reaches a new high score
-        if(score > highScore)
+        if (score > highScore)
         {
             highScore = score;
             _scoreText.text = "High Score: " + highScore;
         }
+
+        // Press 'B' to reset high score (for test)
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ResetHighScore();
+        }
+
     }
 
     // Saves high score when player dies
     public void SaveScore()
     {
+        Debug.Log("Score saved: " + highScore);
+
         PlayerPrefs.SetInt("score", highScore);
+        PlayerPrefs.Save();
+    }
+
+    // Resets high score (for test)
+    public void ResetHighScore()
+    {
+        highScore = 0;
+        score = 0;
+        PlayerPrefs.SetInt("score", 0);
+        PlayerPrefs.Save();
+        _scoreText.text = "High Score: " + highScore;
+        //Debug.Log("High Score has been reset!");
     }
 }
