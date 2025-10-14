5342c03a3f59acec8f79600748e3b8b8d6eb0db7
MinerSheep <ryderpbnj@gmail.com>
2025-10-14 12:27:54 -0700

    change to flicker light

--- Project0918/Assets/Scripts/FlickerLight.cs
+++ Project0918/Assets/Scripts/FlickerLight.cs
@@ -3,10 +3,9 @@ using System;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 
-[RequireComponent(typeof(FlashLight))]
 public class FlickerLight : MonoBehaviour
 {
-    [HideInInspector] public FlashLight flashlight; // URP 2D light
+    [SerializeField] public GameObject spriteMask;
     [SerializeField] public GameObject spriteMask2;
     public float flickerSpeed = 5f;
     public float flickerAmount = 0.1f;
@@ -15,13 +14,13 @@ public class FlickerLight : MonoBehaviour
 
     void Start()
     {
-        flashlight = GetComponent<FlashLight>();
+        
     }
 
     void Update()
     {
         float noise = Mathf.PerlinNoise(Time.time * flickerSpeed, 0f);
-        flashlight.spriteMask.GetComponent<SpriteMask>().alphaCutoff = Mathf.Clamp01(baseIntensity + (noise - 0.5f) * flickerAmount);
+        spriteMask.GetComponent<SpriteMask>().alphaCutoff = Mathf.Clamp01(baseIntensity + (noise - 0.5f) * flickerAmount);
 
         if (spriteMask2 != null)
         {
