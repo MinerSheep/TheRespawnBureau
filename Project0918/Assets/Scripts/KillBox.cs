e41c8e7c5a39063109e5478f63669dfe772b27e8
MinerSheep <ryderpbnj@gmail.com>
2025-10-21 14:39:42 -0700

    update menu, and autorunners

--- Project0918/Assets/Scripts/KillBox.cs
+++ Project0918/Assets/Scripts/KillBox.cs
@@ -16,7 +16,9 @@ public class KillBox : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            // Optional
             CollectionManager.instance.SaveScore(); // Save high score to PlayerPrefs
+            
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
     }
