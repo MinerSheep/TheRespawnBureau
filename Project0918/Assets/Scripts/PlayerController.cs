613cbd2d965b03eac5fe88c9d700d4f11d552687
Cliff Moore <103685335+TwoSteps555@users.noreply.github.com>
2025-11-20 10:33:32 -0800

    Quick fix for the Sword Attack animation
    Made a quick change that should fix the sword attack animation not showing when it should.
    
    Also commented out some really annoying unnecessary debug messages.

--- Project0918/Assets/Scripts/PlayerController.cs
+++ Project0918/Assets/Scripts/PlayerController.cs
@@ -120,14 +120,14 @@ public class PlayerController : MonoBehaviour
             {
                 Attacking = true;
                 attackVol.enabled = true;
-                Debug.Log("Attack!");
+                //Debug.Log("Attack!");
             }
             if (Attacking)
             {
                 AttackTimer += Time.deltaTime;
                 if (AttackTimer > AttackTimerEnd)
                 {
-                    Debug.Log("Attack Ended");
+                    //Debug.Log("Attack Ended");
                     Attacking = false;
                     attackVol.enabled = false;
                     AttackTimer = 0f;
@@ -152,7 +152,7 @@ public class PlayerController : MonoBehaviour
             RB.linearVelocity = new Vector2(RB.linearVelocity.x, 0);
             RB.AddForce(Vector2.up * DoubleJumpForce, ForceMode2D.Impulse);
             doublejump = false;
-            Debug.Log("Doublejump");
+            //Debug.Log("Doublejump");
         }
     }
 
@@ -164,7 +164,7 @@ public class PlayerController : MonoBehaviour
             if(inputBuffer.Consume("Jump")&&JumpTimer>0)
             {
                 RB.AddForce(new Vector2(0,JumpHoldForce));
-                Debug.Log("holdjump");
+                //Debug.Log("holdjump");
             }
             else
             {
@@ -189,7 +189,7 @@ public class PlayerController : MonoBehaviour
         else if (Jumping == true && inputBuffer.Consume("Crouch"))
         {
             RB.AddForce(Vector2.down * FallingForce);
-            Debug.Log("SFA");
+            //Debug.Log("SFA");
         }
         if (Crouching)
         {
