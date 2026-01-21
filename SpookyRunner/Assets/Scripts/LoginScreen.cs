390779694423483fc8fe4b34c99cf086469c3abc
MinerSheep <ryderpbnj@gmail.com>
2026-01-21 14:08:38 -0800

    password input box works with conditions

--- SpookyRunner/Assets/Scripts/LoginScreen.cs
+++ SpookyRunner/Assets/Scripts/LoginScreen.cs
@@ -1,15 +1,33 @@
+using TMPro;
 using UnityEngine;
 
 // Login screen is responsible for device id, password, and starting off the update procedure attached to manager
 public class LoginScreen : MonoBehaviour
 {
+    Transform welcomeText;
+    Transform loginText;
+    Transform passwwordInput;
+
+    private string deviceId;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        welcomeText = transform.Find("WelcomeText");
+        loginText = transform.Find("LoginText");
+        passwwordInput = transform.Find("PasswordInput");
+
+        // Set up password detection
+        TMP_InputField ifield = passwwordInput.GetComponent<TMP_InputField>();
+        ifield.onValueChanged.AddListener(OnPasswordChange);
+        ifield.onEndEdit.AddListener(OnPasswordEntered);
+        ifield.characterLimit = 10;
+
         // Retrieve and display device id
+        deviceId = SystemInfo.deviceUniqueIdentifier;
+        welcomeText.GetComponent<TextMeshProUGUI>().text = "Welcome, user " + deviceId;
 
         // Begin bootstrapping and assign loading bar progress to OnProgress
-        UpdateProcedure.instance.BeginBootstrap();
+        //UpdateProcedure.instance.BeginBootstrap();
 
         UpdateProcedure.instance.OnProgress += UpdateLoadingBar;
         UpdateProcedure.instance.OnCompleted += DisplayLogin;
@@ -35,6 +53,23 @@ public class LoginScreen : MonoBehaviour
     void DisplayError(string failReason)
     {
 
+    }
+    
+    void OnPasswordChange(string password)
+    {
+        string errstring = "";
+
+        if (password.Length >= 10)
+        {
+            errstring += "Password cannot be longer than 10 characters.\n";
+        }
+
+        passwwordInput.Find("ErrorText").GetComponent<TextMeshProUGUI>().text = errstring;
+    }
+
+    void OnPasswordEntered(string password)
+    {
+        passwwordInput.Find("ErrorText").GetComponent<TextMeshProUGUI>().text = "Password was entered!";
     }
 
     private void OnDestroy()
