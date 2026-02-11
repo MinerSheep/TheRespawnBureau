4d8645bfd9d180dd754ad58f4d415339a7e601c3
JakeDeLuise <jakedeluise@gmail.com>
2026-02-10 16:40:13 -0800

    Motion lines

--- SpookyRunner/Assets/Scripts/Runner Scene.cs
+++ SpookyRunner/Assets/Scripts/Runner Scene.cs
@@ -18,6 +18,8 @@ public class RunnerScene : MonoBehaviour
 
     public HUD hud;
 
+    public ParticleSystem Speedlines;
+
     // Private variables
     [HideInInspector] public float MovingSpeed;
 
@@ -79,6 +81,7 @@ public class RunnerScene : MonoBehaviour
             MovingSpeed = DashSpeed;
             dashTimer = dashDuration;
             hud.StaminaAmount -= 15f;
+            Speedlines.gameObject.SetActive(true);
         }
         
         dashTimer -= Time.deltaTime;
@@ -88,6 +91,7 @@ public class RunnerScene : MonoBehaviour
             dashTimer = 0;
             canDash = true;
             MovingSpeed = StartMovingSpeed;
+            Speedlines.gameObject.SetActive(false);
         }
         
         if(MovingSpeed < MinimumMovingSpeed)
